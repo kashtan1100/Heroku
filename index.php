37da7c1f7e4232350a4fd2ddaeb0c7ac00f8b28d
Kashtan <otabekxama822@gmail.com>
2020-08-16 13:12:13 +0300

    resgf

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 include ('vendor/autoload.php');
 include ('menu.php');
+include ('yandex.php');
 use Telegram\Bot\Api;
 
 $telegram = new Api('1012626756:AAEfnux4jc_W_O8f0x87_L6ejWMMK9wvPhc');
@@ -35,3 +36,30 @@ else if ($text == "Кнопка 2"){
     $telegram->sendMessage(['chat_id' => $chat_id , 'text' => $reply,
         'reply_markup' => $reply_markup]);
 }
+else if ($text == "42") {
+    $img = 'https://project42.space/42.jpg';
+    $reply = "Hello " . $first_name . " " . $last_name;
+    $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $menu, 'resize_keyboard' => true, 'one_time_keyboard' => false]);
+    $telegram->sendPhoto(['chat_id' => $chat_id, 'photo' => $img, 'caption' => $reply, 'parse_mode' => 'HTML']);
+}
+
+else if ($text == "Новости") {
+    $xml = simplexml_load_file('https://news.google.com/rss/topics/CAAqKAgKIiJDQkFTRXdvSkwyMHZNR1ptZHpWbUVnSnlkUm9DVWxVb0FBUAE?hl=ru&gl=RU&ceid=RU%3Aru');
+    $i = 0;
+    $reply = "Наука и технологии: \n\n";
+    foreach ($xml->channel->item as $item) {
+        $i++;
+        if ($i > 10) {
+            break;
+        }
+        $reply .= "\xE2\x9E\xA1 " . $item->title . "\nДата: " .
+            $item->pubDate . "(<a href='" . $item->link . "'>Читать полностью</a>)\n\n";
+    }
+    $telegram->sendMessage(['chat_id' => $chat_id, 'parse_mode' => 'HTML',
+        'disable_web_page_preview' => true, 'text' => $reply]);
+}
+
+else if (explode(' ', $text)[0] == "/tr") {
+    yandex_translate();
+}
+
